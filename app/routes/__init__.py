03e61bcd6c8c743b880359f107b7b83a3fec1056
Sourab <sharmasourab93@gmail.com>
2020-09-14 22:09:04 +0530

    Routes __init__.py Progress as of 14/09/2020

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,4 +1,17 @@
-from app.routes import home_ui, login_routes, api
+from app import app
+from app.routes.api import UserAPI
+from app.routes.home_ui import HomePage
+from app.routes.login_routes import SignUp
 
 
-__all__ = ['home_ui', 'login_routes', 'api']
+# Architecture Plan 1: /api/
+app.add_url_rule("/api/", view_func=UserAPI.as_view("user_api"))
+
+# Architecture Plan 2: /home/ & Template View
+app.add_url_rule("/home/", view_func=HomePage.as_view("home_view"))
+
+# Architectures Plan 3: Login Routes URL Views
+app.add_url_rule("/signup/", view_func=SignUp.as_view("signup"))
+app.add_url_rule("/login/", view_func=SignUp.as_view("login"))
+app.add_url_rule("/logout/", view_func=SignUp.as_view("logout"))
+#TODO: Add URL rule for password-change
