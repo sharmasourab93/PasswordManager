de6d4b7c4cf6f7a06a510f5401a82a5e29919008
Sourab <sharmasourab93@gmail.com>
2020-09-14 22:11:12 +0530

    App Init & main

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 # Flask Related Imports
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
@@ -9,16 +9,22 @@ from configs.load_configs import CONN_STRING
 # Log Configuration import
 from log import LogConfigurator
 
+# Core Python related imports
+from os.path import join, abspath, dirname, curdir
+
 
 LogConfigurator.setup_logging()
 logger = LogConfigurator.get_logger(__name__)
 logger.info("Initializing Password Manager")
 
 
-app = Flask(__name__)
+template_dir = join(curdir, 'app/templates')
+
+app = Flask(__name__, template_folder=template_dir)
 app.config['SQLALCHEMY_DATABASE_URI'] = CONN_STRING
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 app.secret_key = '177d30a6751dd7f5bd81babbe8ce3aa2'
+
 
 db = SQLAlchemy(app)
 
@@ -30,4 +36,9 @@ login_manager.login_message_category = 'info'
 # Models Initiation Import.
 from app.models import initialize_db
 
+
 initialize_db()
+
+
+# To import URL/Routes & it's corresponding views.
+from app import routes
--- app/__main__.py
+++ app/__main__.py
@@ -5,4 +5,4 @@ from app.log.log_configurator import LogConfigurator
 LogConfigurator.setup_logging()
 logger = LogConfigurator.get_logger(__name__)
 logger.info("Server Started")
-application.run(host="0.0.0.0", port=5000)
+application.run(host="0.0.0.0", port=5000, debug=True)
